ffa22c7f92e52efd50f90eb6b9578dfb69325a2a
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-08-25 16:57:20 -0400

    s

--- src/services/aiServices.js
+++ src/services/aiServices.js
@@ -2,26 +2,16 @@ const OpenAI = require("openai");
 require("dotenv").config();
 const promptSystem = require("../utils/promptSystem");
 
-<<<<<<< Updated upstream
-const openai = new OpenAI({
+const aiClient = new OpenAI({
         baseURL: process.env.AI_API_URL,
         apiKey: process.env.AI_API_KEY,
-=======
-const aiClient = new OpenAI({
-        baseURL: process.env.AI_API_URL_LOCAL,
-        apiKey: process.env.AI_API_KEY_LOCAL,
->>>>>>> Stashed changes
 });
 
 async function aiGeneration(prompt) {
   const completion = await aiClient.chat.completions.create({
     messages: [{ role: "system", content: promptSystem }, 
                 { role: "user", content: prompt }],
-<<<<<<< Updated upstream
     model: process.env.AI_MODEL,
-=======
-    model: process.env.AI_MODEL_LOCAL,
->>>>>>> Stashed changes
   });
   console.log(completion);
   return completion.choices[0].message.content
