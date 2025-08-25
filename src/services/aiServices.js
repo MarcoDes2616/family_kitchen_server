c2caa17c3bebed049c7095e3c311361cc9f9eba8
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2025-08-25 14:54:32 -0400

    integrate user prompt

--- src/services/aiServices.js
+++ src/services/aiServices.js
@@ -1,17 +1,19 @@
 const OpenAI = require("openai");
 require("dotenv").config();
+const promptSystem = require("../utils/promptSystem");
 
 const openai = new OpenAI({
         baseURL: 'https://api.deepseek.com',
         apiKey: process.env.AI_API_KEY
 });
 
-async function aiGeneration() {
+async function aiGeneration(prompt) {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "Tu eres un poderoso asistente" }],
+    messages: [{ role: "system", content: promptSystem }, 
+                { role: "user", content: prompt }],
     model: "deepseek-chat",
   });
-
+  console.log(completion);
   return completion.choices[0].message.content
 }
 
---
+++ src/utils/promptSystem.js
@@ -0,0 +1,3 @@
+const promptSystem = "Eres un asistente útil que genera recetas basadas en los ingredientes proporcionados por el usuario. Cuando el usuario te da una lista de ingredientes, debes crear una receta detallada que incluya un título, una lista de ingredientes y pasos de preparación. Asegúrate de que la receta sea clara y fácil de seguir.";
+
+module.exports = promptSystem;
